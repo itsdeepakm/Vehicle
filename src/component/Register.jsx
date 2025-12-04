555b6e040c9cff5e646a1521a19e66ad1a1f89e6
itsdeepakm <deepaklewiskumar.19@gmail.com>
2025-12-04 12:21:30 +0530

    name-validations

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -1,19 +1,60 @@
 import "./Register.css"
-
+import { useState } from "react";
 export default function Register() {
+    const details={
+        name:"",
+        phone:"",
+        email:"",
+        password:"",
+        confirmpassword:"",
+        role:"" 
+     }
+    const [error,seterror]=useState({});
+    const [data,setdata]=useState(details);
+    function validate(name,value){
+        let message="";
+        switch(name){
+            case "name":
+                if(!value.trim()){
+                    message="name is required"
+                }
+                else if (!/^[A-Za-z\s]+$/.test(value)){
+                    message="name must contain only alphabets";
+                }
+                else if(value.length<3){
+                    message="name must be atleast 3 character"
+                }
+                break;
+
+
+        }
+        seterror((prev)=>({...prev,[name]:message}));
+           
+    }
+  function handleinputchange(e){
+    let {name,value}=e.target;
+    setdata((prev)=>({...prev,[name]:value}))
+    validate(name,value);
+  }
   return (
     <div>   
     <div className="registerdiv">
        <h2>Register page</h2>
-       <input type="text" placeholder="Name"className="input-field" />
+       <input type="text"name="name" placeholder="Name"className="input-field"onChange={(e)=>handleinputchange(e)} />
+       <div>{error && <p>{error.name}</p>}</div>
+       <input type="text"name="phone" placeholder="Phone Number"className="input-field" />
        <div></div>
-       <input type="text" placeholder="Phone Number"className="input-field" />
+       <input type="email"name="email" placeholder="Email" className="input-field"/>
        <div></div>
-       <input type="email" placeholder="Email" className="input-field"/>
+       <input type="password"name="password" placeholder="Password" className="input-field" />
        <div></div>
-       <input type="password" placeholder="Password" className="input-field" />
+       <input type="password"name="confrimpassword" placeholder="Confirm Password" className="input-field"/>
        <div></div>
-       <input type="password" placeholder="Confirm Password" className="input-field"/>
+       <select className="input-field"name="role">
+        <option value="" >Select Role</option>
+        <option value="Customer">Customer</option>
+        <option value="admin">Admin</option>
+       </select>
        <div></div>
        <button className="btn">Register</button>
     </div>
