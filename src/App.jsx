f4ea8b96702c9a7d5ab6a80080473d06354509ba
itsdeepakm <deepaklewiskumar.19@gmail.com>
2025-12-05 12:38:18 +0530

    add vehcile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Register from './component/Register'
 import './App.css'
 import { Route, Routes, Navigate } from "react-router-dom";
 import Login from "../src/component/Login";
+import AddVehicle from './component/AddVehicle';
 function App() {
   
 
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Navigate to="/homepage" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/addvehicle" element={<AddVehicle/>}/>
     </Routes>
     </>
   )
