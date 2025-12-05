29e022043914ab28df40309647e8c9d1084cd695
itsdeepakm <deepaklewiskumar.19@gmail.com>
2025-12-05 12:08:33 +0530

    show card popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 
 import Register from './component/Register'
 import './App.css'
-
+import { Route, Routes, Navigate } from "react-router-dom";
+import Login from "../src/component/Login";
 function App() {
   
 
   return (
     <>
-    <Register/>
+    <Routes>
+      <Route path="/" element={<Navigate to="/homepage" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
     </>
   )
 }
